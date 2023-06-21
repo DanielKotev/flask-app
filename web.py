7eb4d948cb7312221c639e9caf9d745638862391
DanielKotev <Danielkotev@gmail.com>
2023-06-21 17:32:44 +0300

    get secrets

--- web.py
+++ web.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python 
-from flask import Flask 
-from flask import render_template 
-import os 
+from flask import Flask
+from flask import render_template
+import os
+import boto3
+import json
 
-app = Flask(__name__) 
+app = Flask(__name__)
+env = os.getenv("ENV")
+def get_env_secret():
+    client = boto3.client('secretsmanager')
+    response = client.get_secret_value(
+        SecretId='password'
+    )
+    env_password = json.loads(response['SecretString'])
+    if env == 'prod':
+        print(env_password['env_prod'])
+    if env == 'stage':
+        print(env_password['env_stage'])
+    if env == 'dev':
+        print(env_password['env_dev'])
 
-env=os.getenv("ENV")
-@app.route("/env") 
-def home(): return f"Hello Nick, Its {env} enviroment"
 
-@app.route("/picture") 
+@app.route("/env")
+def home(): return f"This is {env} and the password is {get_env_secret()}"
+
+
+@app.route("/picture")
 def pic():
-    return render_template('index.html') 
+    return render_template('index.html')
 
-@app.route("/pragmatic") 
-def salvador(): 
-    return f"hello from {env}" 
 
-if __name__ == "__main__": 
-    app.run(host='0.0.0.0',debug=True)
+@app.route("/pragmatic")
+def salvador():
+    return f"hello from {env}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
